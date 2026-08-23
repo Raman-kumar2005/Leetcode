5fe0ad4c20f7cfb63d3f3c15b981b63e21dec854
Raman Kumar <ramankumardilip2005@gmail.com>
2026-08-23 11:48:57 +0530

    feat: add Sum Game solution

--- algorithms/sum-game/solution.py
+++ algorithms/sum-game/solution.py
@@ -1,10 +1,20 @@
 class Solution:
-    def checkDivisibility(self, n: int) -> bool:
-        m=str(n)
-        l=list(map(int,m))
-        digit_sum=0
-        digit_product=1
-        for i in l:
-            digit_sum+=i
-            digit_product*=i
-        return n%(digit_sum+digit_product)==0
+    def sumGame(self, nums: str) -> bool:
+        length = len(nums)
+        my_list = list(nums)
+        def digit_sum(half_list):
+            total = 0
+            q_count = 0
+            for char in half_list:
+                if char == "?":
+                    q_count += 1
+                else:
+                    total += int(char)
+            return total, q_count
+       
+        left_sum, left_q = digit_sum(my_list[:length//2])
+        right_sum, right_q = digit_sum(my_list[length//2:])
+        if 2 * left_sum + 9 * left_q == 2 * right_sum + 9 * right_q:
+            return False
+        else:
+            return True
