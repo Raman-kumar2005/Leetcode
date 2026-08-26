8b836e0af8de18ff2435f247e872362c5646f837
Raman Kumar <ramankumardilip2005@gmail.com>
2026-08-26 19:03:23 +0530

    feat: add Shortest and Lexicographically Smallest Beautiful String solution

--- algorithms/shortest-and-lexicographically-smallest-beautiful-string/solution.py
+++ algorithms/shortest-and-lexicographically-smallest-beautiful-string/solution.py
@@ -1,11 +1,20 @@
 class Solution:
-    def missingMultiple(self, nums: List[int], k: int) -> int:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
         l=[]
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                l.append(s[i:j+1])
+        beautiful_sub=[]
+        for i in l:
+            if i.count("1")==k:
+                beautiful_sub.append(i)
+        if not beautiful_sub:
+            return ""
+        min_len = min(len(x) for x in beautiful_sub)
+        shortest_subs = [x for x in beautiful_sub if len(x) == min_len]
+        return min(shortest_subs)
+       
         
+
+
         
-        for i in nums:
-            if i%k==0:
-                l.append(i)
-        for i in range(k,201,k):
-            if i not in l:
-                return i
