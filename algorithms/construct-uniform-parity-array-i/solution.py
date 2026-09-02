45977dedc2df25366bdea52e2b994b36ce048a55
Raman Kumar <ramankumardilip2005@gmail.com>
2026-09-02 21:41:09 +0530

    feat: add Construct Uniform Parity Array I solution

--- algorithms/construct-uniform-parity-array-i/solution.py
+++ algorithms/construct-uniform-parity-array-i/solution.py
@@ -1,28 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        l=[]
-        temp=head
-        while temp:
-            l.append(temp.val)
-            temp=temp.next
-        m=[]
-        for i in range(1,len(l)-1):
-            if l[i-1]<l[i]>l[i+1]:
-                m.append(i)
-            elif l[i-1]>l[i]<l[i+1]:
-                m.append(i)
-        if len(m)<2:
-            return [-1,-1]
-        
-        min_dis=m[1]-m[0]
-        for i in range(1,len(m)-1):
-            if m[i+1]-m[i]<min_dis:
-                min_dis= m[i+1]-m[i]
-            
-
-        return [min_dis,m[-1]-m[0]]
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
