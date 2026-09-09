016e145f7103f448585f235c2d6c325b1735370a
Raman Kumar <ramankumardilip2005@gmail.com>
2026-09-09 18:04:36 +0530

    feat: add Count Commas in Range solution

---
+++ algorithms/count-commas-in-range/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n<1000){
+            return 0;
+        }
+        int result =0;
+        for (int i=1000; i<=n; i++){
+            int len=String.valueOf(Math.abs(i)).length();
+            int temp= (int) (len-1)/3;
+            result+=temp;
+        }
+        
+        return result;
+
+    }
+
+}
