28089ad48805d61d48c65931e4fe0f35b51ddccc
Raman Kumar <ramankumardilip2005@gmail.com>
2026-09-20 14:03:00 +0530

    feat: add Find Customer Referee solution

--- algorithms/find-customer-referee/solution.sql
+++ algorithms/find-customer-referee/solution.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select product_id from Products
-where low_fats="Y" and recyclable ="Y";
+select name from Customer 
+where referee_id!=2 or referee_id is null;
