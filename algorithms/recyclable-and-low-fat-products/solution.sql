51222da3c9f9eecfa32b0ff01361a8640f7528e3
Raman Kumar <ramankumardilip2005@gmail.com>
2026-09-20 13:55:01 +0530

    feat: add Recyclable and Low Fat Products solution

--- algorithms/recyclable-and-low-fat-products/solution.sql
+++ algorithms/recyclable-and-low-fat-products/solution.sql
@@ -1,7 +1,3 @@
 # Write your MySQL query statement below
-select  p.firstName,
-    p.lastName,
-    a.state,a.city
-    from person p
-    left join address a
-    on p.personId=a.personId
+select product_id from Products
+where low_fats="Y" and recyclable ="Y";
